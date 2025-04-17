9483bc04f15f15c53a7c954928f231f670d925d0
jaytanakit <tanakit.sutt@psd.co.th>
2025-04-17 16:01:09 +0700

    Edit return value

--- src/main_reports/main_reports.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { MainReportsService } from './main_reports.service';
-
-@Controller('main-reports')
-export class MainReportsController {
-  constructor(private readonly mainReportsService: MainReportsService) {}
-}
--- src/main_reports/main_reports.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MainReportsService } from './main_reports.service';
-import { MainReportsController } from './main_reports.controller';
-
-@Module({
-  controllers: [MainReportsController],
-  providers: [MainReportsService],
-})
-export class MainReportsModule {}
